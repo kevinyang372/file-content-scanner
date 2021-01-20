30650d9258a8b869469a053f72fa06d564440259
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2021-01-20 12:41:55 -0800

    refactor scan_file in a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
 extern crate clap;
 use clap::App;
 use std::collections::VecDeque;
-use std::io::{self, Write};
+use std::io;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let stdout = io::stdout();
-    let mut handle = io::BufWriter::new(stdout);
-    let matches = App::new("fc")
-                        .version("1.0")
-                        .author("Kevin Y. <yunfan.yang.kevin@gmail.com>")
-                        .about("Display file content in command line interface")
-                        .args_from_usage(
-                            "<INPUT>            'Sets the input file to use'
-                            -t, --tail=[TAIL]   'Sets the tail number'
-                            -h, --head=[HEAD]   'Sets the head number'")
-                        .get_matches();
-
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let input_file = matches.value_of("INPUT").unwrap();
-    let tail_number = matches.value_of("tail").unwrap_or("0").parse::<usize>().unwrap();
-    let head_number = matches.value_of("head").unwrap_or("0").parse::<usize>().unwrap();
-
-    let content = std::fs::read_to_string(&input_file)?;
+fn scan_file(content: &str, mut handle: impl std::io::Write,head_number: &usize, tail_number: &usize) -> Result<(), Box<dyn std::error::Error>> {
     let mut i = 1;
-
     let mut tail_queue = VecDeque::new();
+
+    let head_number = *head_number;
+    let tail_number = *tail_number;
 
     if head_number > 0 {
         writeln!(handle, "[HEAD]")?;
@@ -56,4 +40,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
+    let matches = App::new("fc")
+                        .version("1.0")
+                        .author("Kevin Y. <yunfan.yang.kevin@gmail.com>")
+                        .about("Display file content in command line interface")
+                        .args_from_usage(
+                            "<INPUT>            'Sets the input file to use'
+                            -t, --tail=[TAIL]   'Sets the tail number'
+                            -h, --head=[HEAD]   'Sets the head number'")
+                        .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let input_file = matches.value_of("INPUT").unwrap();
+    let tail_number = matches.value_of("tail").unwrap_or("0").parse::<usize>().unwrap();
+    let head_number = matches.value_of("head").unwrap_or("0").parse::<usize>().unwrap();
+
+    let content = std::fs::read_to_string(&input_file)?;
+
+    scan_file(&content, &mut handle, &head_number, &tail_number)
 }
